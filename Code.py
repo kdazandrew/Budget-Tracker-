d43c36399dc9e55003406c2278dc8306a6fde132
cmediate54 <51347390+cmediate54@users.noreply.github.com>
2019-07-18 14:38:05 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,1 +1,20 @@
+import sqlite3 as db 
 
+
+def init():
+  '''
+  Inititalize a new database to store the 
+  expedenitures 
+  '''
+  connect = db.connect("spent.db") 
+  cur = conn.cursor()
+  sql = '''
+  create table if not exists expenses (
+  amount number, 
+  category string, 
+  date string
+  )
+  '''
+  cur.execute(sql)
+  conn.commit()
+init()
