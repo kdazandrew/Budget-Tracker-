2580cf97b28c1146aefdbf046beabdca0f54fb77
cmediate54 <51347390+cmediate54@users.noreply.github.com>
2019-07-18 14:56:36 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,20 +1,39 @@
-import sqlite3 as db 
+import os
+import sys
 
+class Application():
+    def __init__(self):
+        self.income = 0
+        self.expenses = 0
+        self.expense_list = []
+        self.expense_name = []
+        self.income_name = []
+        self.income_list = []
+        self.prompt_income()
 
-def init():
-  '''
-  Inititalize a new database to store the 
-  expedenitures 
-  '''
-  connect = db.connect("spent.db") 
-  cur = conn.cursor()
-  sql = '''
-  create table if not exists expenses (
-  amount number, 
-  category string, 
-  date string
-  )
-  '''
-  cur.execute(sql)
-  conn.commit()
-init()
+    def income_ask(self):
+        add_income = input('Add income? [y/n]: ')
+        return add_income
+
+    def income_sum(self):
+        self.income = sum(self.income_list)
+
+    def expense_ask(self):
+        add_expense = input('Add expense? [y/n]: ')
+        return add_expense
+
+    def expense_sum(self):
+        self.expenses = sum(self.expense_list)
+
+    def income_check(self):
+        if not self.income_list:
+            print('Please enter atleast one source of income. ')
+            self.prompt_income()
+        else:
+            return
+
+    def expense_check(self):
+        if not self.expense_list:
+            print('Please enter atleast one expense. ')
+            self.prompt_expense()
+        else:
