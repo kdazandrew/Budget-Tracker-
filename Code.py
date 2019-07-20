a86db67f5da30fc6f25b66874f6e75a6e2bc3ec0
cmediate54 <51347390+cmediate54@users.noreply.github.com>
2019-07-19 23:22:25 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,39 +1,33 @@
-import os
-import sys
+expense = 0.0
+budget = 0.0
+difference = 0.0
+expenseTotal = 0.0
 
-class Application():
-    def __init__(self):
-        self.income = 0
-        self.expenses = 0
-        self.expense_list = []
-        self.expense_name = []
-        self.income_name = []
-        self.income_list = []
-        self.prompt_income()
+total_expense = 0
+keep_going = 'y'
 
-    def income_ask(self):
-        add_income = input('Add income? [y/n]: ')
-        return add_income
 
-    def income_sum(self):
-        self.income = sum(self.income_list)
 
-    def expense_ask(self):
-        add_expense = input('Add expense? [y/n]: ')
-        return add_expense
+budget = float(input("What is your budget for the month?"))
+print("Please begin entering the amounts of each of your monthly expenses:")
 
-    def expense_sum(self):
-        self.expenses = sum(self.expense_list)
+while keep_going == 'y':
+    expense = float(input("Monthly expense amount? $"))
 
-    def income_check(self):
-        if not self.income_list:
-            print('Please enter atleast one source of income. ')
-            self.prompt_income()
-        else:
-            return
+    total_expense = total_expense + expense
+    keep_going = input("Do you have any other expenses? (Enter y for yes.)")
 
-    def expense_check(self):
-        if not self.expense_list:
-            print('Please enter atleast one expense. ')
-            self.prompt_expense()
-        else:
+
+if expense < budget:
+    difference = budget - expense
+    print("You were $", difference, " under budget.")
+
+elif expense > budget:
+    difference = expense - budget
+    print("You were $", difference, " over budget.")
+
+else:
+    print("You were right on budget. Great Job!!!")
+
+
+input("Press enter to exit.")
