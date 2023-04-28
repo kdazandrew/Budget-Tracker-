f345299a05a5a963b1fa86ededb8a1b42b20d901
Andrew Zhang <azhang8@students.kennesaw.edu>
2023-04-28 16:19:48 -0400

    Updated budget tracker
    I made a few changes to Budget Tracker. First, I changed the variable names. The current variable names are not very descriptive. Instead of 'keep_going', I used 'continue_input', and instead of 'total_expense', I used 'sum_of_expenses.' Second, I noticed the variables'difference' and 'expensetotal' are not used in the code and I removed them. Third, I used f strings to format output instead of concatenating strings with varibles using the '+' operator. By using f-strings to format the output it makes the code easier to read and maintain. Lastly, instead of using a while loop with a condition that changes inside the loop, I use while true loop and break out of it when the user is done entering expenses.

--- Code.py
+++ Code.py
@@ -1,33 +1,21 @@
-expense = 0.0
-budget = 0.0
-difference = 0.0
-expenseTotal = 0.0
-
-total_expense = 0
-keep_going = 'y'
-
-
-
-budget = float(input("What is your budget for the month?"))
+budget = float(input("What is your budget for the month? "))
 print("Please begin entering the amounts of each of your monthly expenses:")
 
-while keep_going == 'y':
+sum_of_expenses = 0
+while True:
     expense = float(input("Monthly expense amount? $"))
+    sum_of_expenses += expense
+    continue_input = input("Do you have any other expenses? (Enter y for yes.) ")
+    if continue_input.lower() != 'y':
+        break
 
-    total_expense = total_expense + expense
-    keep_going = input("Do you have any other expenses? (Enter y for yes.)")
-
-
-if expense < budget:
-    difference = budget - expense
-    print("You were $", difference, " under budget.")
-
-elif expense > budget:
-    difference = expense - budget
-    print("You were $", difference, " over budget.")
-
+if sum_of_expenses < budget:
+    difference = budget - sum_of_expenses
+    print(f"You were ${difference:.2f} under budget.")
+elif sum_of_expenses > budget:
+    difference = sum_of_expenses - budget
+    print(f"You were ${difference:.2f} over budget.")
 else:
     print("You were right on budget. Great Job!!!")
-
 
 input("Press enter to exit.")
